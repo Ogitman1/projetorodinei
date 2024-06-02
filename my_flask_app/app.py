e6b8d673d8c478ef082d26efee4967ceddab9381
Matheus Santos <matheussfelipe.santos@gmail.com>
2024-06-02 18:19:24 -0300

    Adição de script para por o projeto online

--- my_flask_app/app.py
+++ my_flask_app/app.py
@@ -6,6 +6,8 @@ app = Flask(__name__)
 app.register_blueprint(routes_blueprint)
  
 if __name__ == '__main__':
+    from waitress import serve
+    serve(app, host='0.0.0.0', port=8080)
     app.run(debug=True)
 
 
